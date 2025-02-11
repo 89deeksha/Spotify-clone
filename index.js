2c333d9608918679c6dd6579e15ee7296a96f543
89deeksha <deekshajain0310@gmail.com>
2025-02-11 14:39:18 +0530

    tunes

--- index.js
+++ index.js
@@ -15,10 +15,38 @@ console.log("deeksha")
 // audioElement.play()
 // initialize the variable
 let songIndex=0;
-let audioElement=new Audio('./songs/1.mp3')
+let audioElement=new Audio('songs/1.mp3')
 let masterplay=document.getElementById('masterplay')
 let masterprogressbar=document.getElementById('myprogressbar')
 let gif=document.getElementById('gif')
+let songitems=Array.from(document.getElementsByClassName('songitem'))
+console.log(songitems)
+
+
+const makeallplays=()=>{
+    Array.from(document.getElementsByClassName('songitem-play')).forEach((element)=>{
+    element.classList.add('fa-play')
+    element.classList.add('fa-pause')
+    
+
+})
+}
+
+Array.from(document.getElementsByClassName('songitem-play')).forEach((element)=>{
+    element.addEventListener('click',(e)=>{
+        
+        makeallplays()
+        index=parseInt(e.target.id)
+        e.target.classList.remove('fa-play')
+        e.target.classList.add('fa-pause')
+        audioElement.src=`./songs/${index+1}.mp3`
+        audioElement.play()
+        masterplay.classList.remove('fa-play')
+        masterplay.classList.add('fa-pause')
+    })
+})
+
+
 
 
 
@@ -30,13 +58,24 @@ let gif=document.getElementById('gif')
 
 
 let song=[ //here is an array object of songname,filepath,coverpath
-    {songName:"love-me-like",filepath:"songs/1.mp3",coverpath:"cover/1.jpg"},
-    {songName:"love-me-like",filepath:"songs/1.mp3",coverpath:"cover/1.jpg"},
-    {songName:"love-me-like",filepath:"songs/1.mp3",coverpath:"cover/1.jpg"},
-    {songName:"love-me-like",filepath:"songs/1.mp3",coverpath:"cover/1.jpg"},
-    {songName:"love-me-like",filepath:"songs/1.mp3",coverpath:"cover/1.jpg"},
-    {songName:"love-me-like",filepath:"songs/1.mp3",coverpath:"cover/1.jpg"}
+    {songName:"Tune-1",filepath:"./songs/1.mp3",coverpath:"covers/1.jpg"},
+    {songName:"Tune-2",filepath:"./songs/2.mp3",coverpath:"covers/2.jpg"},
+    {songName:"Tune-3",filepath:"./songs/3.mp3",coverpath:"covers/3.jpg"},
+    {songName:"Tune-4",filepath:"./songs/4.mp3",coverpath:"covers/4.jpg"},
+    {songName:"Tune-5",filepath:"./songs/5.mp3",coverpath:"covers/5.jpg"},
+    {songName:"Tune-6",filepath:"./songs/6.mp3",coverpath:"covers/6.jpg"}
 ]
+
+
+
+
+
+songitems.forEach((element,i)=>{
+   
+    element.getElementsByTagName("img")[0].src=song[i].coverpath
+    element.getElementsByClassName("songName")[0].innerText=song[i].songName
+    
+})
 
 // handleplay/pause
 masterplay.addEventListener('click',()=>{ //here we put addeventlistners on playbutton
@@ -44,7 +83,7 @@ masterplay.addEventListener('click',()=>{ //here we put addeventlistners on play
         audioElement.play() //if pause then it will be play by clicking play
         masterplay.classList.remove('fa-play') //if we click play then it remove play
         masterplay.classList.add('fa-pause') //and put pause instead
-        gif.style.opacity=1
+        gif.style.opacity=1 //opacity for gif will be one
         
         
 
@@ -69,7 +108,7 @@ audioElement.addEventListener('timeupdate',()=>{ //here on audioelement we put t
     }
     
 )
-masterprogressbar.accessKeyLabel('change',()=>{
-    audioElement.currentTime=masterprogressbar.value
+masterprogressbar.addEventListener('change',()=>{ //when we change masterprogressbar it will also change the audio
+    audioElement.currentTime=masterprogressbar.value*audioElement.duration/100
 })
 
